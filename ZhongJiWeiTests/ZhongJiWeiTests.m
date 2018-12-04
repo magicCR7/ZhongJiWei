4a7c7c308b032593ef545a302170269e82db3e70
Mc <940563060@qq.com>
2018-12-04 13:18:47 +0800

    Initial Commit

---
+++ ZhongJiWeiTests/ZhongJiWeiTests.m
@@ -0,0 +1,37 @@
+//
+//  ZhongJiWeiTests.m
+//  ZhongJiWeiTests
+//
+//  Created by Mc on 2018/12/4.
+//  Copyright © 2018年 Mc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ZhongJiWeiTests : XCTestCase
+
+@end
+
+@implementation ZhongJiWeiTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
